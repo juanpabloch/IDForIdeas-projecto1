97b704df8ea0518d998cc1d25171f47f5c7c4b45
juanpabloch <63684218+juanpabloch@users.noreply.github.com>
2021-12-31 14:29:07 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(cors());
+
+app.use('/', routes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'page not found' });
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port: ${port}`);
+});
